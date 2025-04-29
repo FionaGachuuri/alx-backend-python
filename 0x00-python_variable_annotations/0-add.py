54e8aebaaeae839848b98e30ed7868fc4dc889f2
FionaGachuuri <fionairuka@gmail.com>
2025-04-29 13:43:00 +0300

    Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This script defines a function to add two numbers together.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two numbers together.
+
+    Args:
+        a (float): The first number.
+        b (float): The second number.
+
+    Returns:
+        float: The sum of the two numbers.
+    """
+    return a + b
