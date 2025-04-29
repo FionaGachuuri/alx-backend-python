1481e5c992de7452506653585a9e8d980ccf5bf4
FionaGachuuri <fionairuka@gmail.com>
2025-04-29 13:50:31 +0300

    Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import math
+from typing import Union
+
+"""
+This script defines a function to compute the floor of a number.
+"""
+
+
+def floor(n: Union[float]) -> float:
+    """
+    Computes the floor of a number.
+
+    Args:
+        n (float): The number to compute the floor of.
+
+    Returns:
+        float: The floor of the number.
+    """
+    return math.floor(n)
